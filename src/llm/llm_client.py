6dc0a72cca19de7e185c99da539e03c2f1019610
tuananh26-09 <tnt436587@gmail.com>
2026-06-24 11:27:45 +0700

    Update llm_client.py

--- src/llm/llm_client.py
+++ src/llm/llm_client.py
@@ -1,30 +1,28 @@
-# import os
-# from langchain_openai import ChatOpenAI
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-
-# def get_llm():
-#     return ChatOpenAI(
-#         api_key=os.environ["GROQ_API_KEY"],
-#         base_url="https://api.groq.com/openai/v1", 
-#         model=os.getenv("llm_model"),
-#         temperature=0.0, 
-#         max_tokens=8192
-#     )
-
-
 import os
+from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
+# os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+
+# def get_llm():
+#     return ChatOpenAI(
+#         # api_key=os.environ["GROQ_API_KEY"],
+#         api_key="",
+#         base_url="https://api.groq.com/openai/v1", 
+#         # model=os.getenv("llm_model"),
+#         model="llama-3.1-8b-instant",
+#         temperature=0.1, 
+#         max_tokens=8192
+#     )
+
+
 def get_llm():
     return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-1.5-flash",
         temperature=0.1,
-        google_api_key=os.getenv("GOOGLE_API_KEY")
+        max_tokens=8192,
+        google_api_key=os.getenv("GOOGLE_API_KEY2")
     )
