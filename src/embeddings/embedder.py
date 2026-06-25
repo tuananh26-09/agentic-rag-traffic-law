11dcaa3fb8c8e639d91218f3d379f8c0395a8a88
tuananh26-09 <tnt436587@gmail.com>
2026-06-25 10:59:30 +0700

    Update embedder.py

--- src/embeddings/embedder.py
+++ src/embeddings/embedder.py
@@ -1,8 +1,17 @@
 import os
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
+# def get_embeddings():
+#     return HuggingFaceEmbeddings(model_name=os.getenv("embedding_model"))
+
 def get_embeddings():
-    return HuggingFaceEmbeddings(model_name=os.getenv("embedding_model"))
+    return OpenAIEmbeddings(
+        model="BAAI/bge-m3",
+        openai_api_base="http://embedder_api:7997", 
+        openai_api_key="sk-local", # API nội bộ
+        check_embedding_ctx_length=False,
+    )
