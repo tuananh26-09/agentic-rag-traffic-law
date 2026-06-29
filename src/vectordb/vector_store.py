9fc716071ea5d18974feece92222b25c4f1a9c4c
tuananh26-09 <tnt436587@gmail.com>
2026-06-29 13:48:29 +0700

    Update vector_store.py

--- src/vectordb/vector_store.py
+++ src/vectordb/vector_store.py
@@ -17,3 +17,19 @@ def get_retriever():
     
     # Trả về retriever lấy top * tài liệu
     return vector_db.as_retriever(search_kwargs={"k": 10})
+
+def upload_to_qdrant(all_docs):
+        print(" Đang nạp Vector vào QDRANT trên Docker...")
+        try:
+            embeddings = get_embeddings()
+            
+            QdrantVectorStore.from_documents(
+                documents=all_docs,
+                embedding=embeddings,
+                url=os.getenv("QDRANT_URL"),
+                collection_name="luat_giao_thong_viet_nam",
+                force_recreate=False
+            )
+            print(f" HOÀN TẤT! Hãy mở {os.getenv('QDRANT_URL')}/dashboard để xem dữ liệu.")
+        except Exception as e:
+            print(f" Lỗi khi nạp vào Qdrant: {e}")
